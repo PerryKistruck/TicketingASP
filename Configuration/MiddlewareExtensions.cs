478b736bbee9997336c4294db417bf303bd2b489
Perry Kistruck <perry.kistruck@gmail.com>
2025-12-18 04:34:54 +0000

    feat: Add Content Security Policy to middleware and create web.config for custom headers

--- Configuration/MiddlewareExtensions.cs
+++ Configuration/MiddlewareExtensions.cs
@@ -38,7 +38,7 @@ public static class MiddlewareExtensions
             {
                 context.Response.Headers.Append(
                     "Content-Security-Policy",
-                    "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; connect-src 'self'; frame-ancestors 'none'"
+                    "default-src 'self'; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; img-src 'self' data:; font-src 'self' data: https://cdn.jsdelivr.net; connect-src 'self'; frame-ancestors 'none'"
                 );
             }
 
